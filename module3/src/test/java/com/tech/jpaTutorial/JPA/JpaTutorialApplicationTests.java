1422ce1b730ffcf4c147afec313aeebe56ca5896
Aman Kumar <amankumar93578@gmail.com>
2025-12-15 22:09:31 +0530

    initial commit

---
+++ module3/src/test/java/com/tech/jpaTutorial/JPA/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.jpaTutorial.JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
