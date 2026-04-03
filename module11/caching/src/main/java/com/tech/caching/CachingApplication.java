16f87079ffb5c5468a0ab57107635f0db542833f
gitcoder-aman <amankumar93578@gmail.com>
2026-04-03 21:07:14 +0530

    initial commit

---
+++ module11/caching/src/main/java/com/tech/caching/CachingApplication.java
@@ -0,0 +1,13 @@
+package com.tech.caching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingApplication.class, args);
+	}
+
+}
---
+++ module11/caching/src/test/java/com/tech/caching/CachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
