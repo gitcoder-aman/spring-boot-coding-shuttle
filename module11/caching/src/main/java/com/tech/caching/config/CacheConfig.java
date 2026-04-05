dd32bdd2db15ee48d018853d46fbc6e9416775ba
gitcoder-aman <amankumar93578@gmail.com>
2026-04-05 22:30:58 +0530

    Redis Cache in Spring Boot

--- module11/caching/src/main/java/com/tech/caching/config/CacheConfig.java
+++ module11/caching/src/main/java/com/tech/caching/config/CacheConfig.java
@@ -5,12 +5,40 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.SerializationFeature;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+//    @Bean
+//    public CacheManager cacheManager() {
+//        return new ConcurrentMapCacheManager("employees");
+//    }
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("employees");
+    public CacheManager cacheManager(
+            RedisConnectionFactory connectionFactory) {
+
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(
+                        RedisCacheConfiguration
+                                .defaultCacheConfig()
+                                .prefixCacheNameWith("my-redis-")
+                                .entryTtl(Duration.ofSeconds(60)) // Redis will clear after 60 sec (for updating purpose)
+                                .enableTimeToIdle() // Redis will reset the time of particular data
+//                                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+//                                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJacksonJsonRedisSerializer()))
+                )
+                .build();
     }
 }
--- module11/caching/src/main/java/com/tech/caching/dto/EmployeeDTO.java
+++ module11/caching/src/main/java/com/tech/caching/dto/EmployeeDTO.java
@@ -6,13 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class EmployeeDTO {
+public class EmployeeDTO implements Serializable {
 
     private Long id;
 
