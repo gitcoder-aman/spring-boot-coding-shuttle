b57518137216eda8a90aca80e2c05679fd7ffe30
gitcoder-aman <amankumar93578@gmail.com>
2026-07-20 01:13:47 +0530

    learn docker

---
+++ module15_docker/product-service/src/main/java/com/tech/docker/product_service/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.tech.docker.product_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello From Spring Boot";
+    }
+}
