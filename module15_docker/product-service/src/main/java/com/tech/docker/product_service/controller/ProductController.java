3fb26b5634173e8b11bfb05d2f096e6170d3b5a9
coder-aman <amankumar93578@gmail.com>
2026-07-30 12:25:38 +0530

    learn how to managing multiple docker containers using Docker compose

--- module15_docker/product-service/src/main/java/com/tech/docker/product_service/controller/ProductController.java
+++ module15_docker/product-service/src/main/java/com/tech/docker/product_service/controller/ProductController.java
@@ -1,15 +1,31 @@
 package com.tech.docker.product_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tech.docker.product_service.entity.Product;
+import com.tech.docker.product_service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @GetMapping("/hello")
     public String hello(){
         return "Hello From Spring Boot";
+    }
+
+    @PostMapping
+    public String createProduct(@RequestBody Product product){
+        productService.createProduct(product);
+        return "Product created successfully";
+    }
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
