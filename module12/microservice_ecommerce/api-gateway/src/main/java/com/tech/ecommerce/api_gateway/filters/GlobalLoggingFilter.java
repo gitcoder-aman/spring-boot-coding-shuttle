a70618d067ab05a071e0d2640279b69ac6539752
coder-aman <amankumar93578@gmail.com>
2026-08-05 17:54:09 +0530

    API Gateway filters added

---
+++ module12/microservice_ecommerce/api-gateway/src/main/java/com/tech/ecommerce/api_gateway/filters/GlobalLoggingFilter.java
@@ -0,0 +1,33 @@
+package com.tech.ecommerce.api_gateway.filters;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalLoggingFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        //pre-filter
+        log.info("logging from global pre:{}",exchange.getRequest().getURI());
+        return chain.filter(exchange).then(Mono.fromRunnable(new Runnable() {
+            @Override
+            public void run() {
+                log.info("logging from global post:{}",exchange.getResponse().getStatusCode());
+            }
+        }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 5;
+    }
+}
